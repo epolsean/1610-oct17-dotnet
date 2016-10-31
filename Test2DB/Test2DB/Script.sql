7df08bc9e42e0bbbbb10a2056774fe5ddd8a0175
erik polsean <polseane9067@my.uwstout.edu>
2016-10-31 10:32:39 -0400

    Test2DB

---
+++ Test2DB/Test2DB/Script.sql
@@ -0,0 +1,33 @@
+﻿create schema Costume;
+go
+
+create table Costume.Costume
+(
+    CostumeId int not null identity(1,1)
+    ,Name nvarchar(250) not null
+    ,TypeId int not null
+    ,PicturePath nvarchar(256) null
+	,Complete bit not null
+    ,Active bit not null
+);
+go
+
+create table Costume.CostumeType
+(
+    CostumeTypeId int not null identity(1,1)
+    ,TypeName nvarchar(250) not null
+    ,Active bit not null
+);
+go
+
+alter table Costume.CostumeType
+    add constraint pk_costume_costumetypeid primary key clustered (CostumeTypeId);
+go
+
+alter table Costume.Costume
+    add constraint pk_costume_costumeid primary key clustered (CostumeId);
+go
+
+alter table Costume.Costume
+    add constraint fk_costume_typeid foreign key (TypeId) references Costume.CostumeType (CostumeTypeId);
+go
---
+++ Test2Data/Test2Data.Test/EFDataTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Test2Data.DataAccess;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test2Data.Test
+{
+    public class EFDataTests
+    {
+        [Fact]
+        public void Test_GetCostumes()
+        {
+            var data = new EFData();
+            var actual = data.GetCostumes();
+
+            Assert.NotNull(actual);
+        }
+
+        public void Test_GetCostumeTypes()
+        {
+            var data = new EFData();
+            var actual = data.GetCostumeTypes();
+
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_InsertCostume()
+        {
+            var data = new EFData();
+            Costume costume = new Costume() { Name = "Skelleton", TypeId = 1, Complete = 0, Active = 1};
+            var actual = data.InsertCostume(costume);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Test_InsertCostumeType()
+        {
+            var data = new EFData();
+            CostumeType costumeType = new CostumeType() { Name = "Prop" };
+            var actual = data.InsertCostumeType(costumeType);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Test_UpdateCostume()
+        {
+            var data = new EFData();
+            Costume costume = new Costume() { CostumeId = 3, Name = "Reaper", TypeId = 2, Complete = 1, Active = 1 };
+            var actual = data.ChangeCostume(costume, EntityState.Modified);
+
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_DeleteCostume()
+        {
+            var data = new EFData();
+            Costume costume = new Costume() { CostumeId = 4 };
+            var actual = data.ChangeCostume(costume, EntityState.Deleted);
+
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_CompleteCostume()
+        {
+            var data = new EFData();
+            Costume costume = new Costume() { CostumeId = 2 };
+            var actual = data.ChangeCostume(costume, true);
+
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_IncompleteCostume()
+        {
+            var data = new EFData();
+            Costume costume = new Costume() { CostumeId = 2 };
+            var actual = data.ChangeCostume(costume, false);
+
+            Assert.NotNull(actual);
+        }
+    }
+}
