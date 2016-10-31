7ecd4022ba911c2196195959d0907494203399d7
erik polsean <polseane9067@my.uwstout.edu>
2016-10-31 12:03:49 -0400

    Add Service Project

---
+++ MonsterData/MonsterData.DataClient/MonsterService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MonsterData.DataClient
+{
+    public class MonsterService : IMonsterService
+    {
+        public string DoWork()
+        {
+            return "So clean!";
+        }
+    }
+}
