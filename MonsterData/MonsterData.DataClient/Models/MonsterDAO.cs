547afa0348c844c41b1d912120e34b71c91687f6
erik polsean <polseane9067@my.uwstout.edu>
2016-11-02 11:18:49 -0400

    Fix dependencies

--- MonsterData/MonsterData.DataClient/Models/MonsterDAO.cs
+++ MonsterData/MonsterData.DataClient/Models/MonsterDAO.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace MonsterData.DataClient.Models
 {
+    [DataContract]
     public class MonsterDAO
     {
+        [DataMember]
         public int Id { get; set; }
-        public string Name { get; set;  }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
         public GenderDAO Gender { get; set; }
     }
 }
